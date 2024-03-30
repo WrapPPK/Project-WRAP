7c9454d12d6cb11a4eeae772de454cd81c01dd57
Muhammad Risky Farhan <139313370+Risfrhn@users.noreply.github.com>
2024-03-30 11:25:47 +0800

    Landing Page progres

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('login');
-    return view('ItemNoLogin.pageNoLogin');
+    return view('noLogin.body');
+
 });
